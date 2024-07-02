0bdb4f3d4836881ff68cbcabd66538139e52336e
vatslgoswami <vatslgoswami@gmail.com>
2024-07-03 00:31:00 +0530

    cart functionality added

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,57 +1,83 @@
 const productsGridElement = document.querySelector('.js-products-grid');
 let productsGridHTML = '';
 
-products.forEach((item)=>{
-    productsGridHTML += `
-            <div class="product-container">
-                <div class="product-image-container">
-                    <img class="product-image"
-                    src=${item.image}>
-                </div>
-
-                <div class="product-name limit-text-to-2-lines">
-                    ${item.name}
-                </div>
-
-                <div class="product-rating-container">
-                    <img class="product-rating-stars"
-                    src="images/ratings/rating-${item.rating.stars * 10}.png">
-                    <div class="product-rating-count link-primary">
-                    ${item.rating.count}
+function generateProductGrid(){
+    products.forEach((item)=>{
+        productsGridHTML += `
+                <div class="product-container">
+                    <div class="product-image-container">
+                        <img class="product-image"
+                        src=${item.image}>
                     </div>
-                </div>
+    
+                    <div class="product-name limit-text-to-2-lines">
+                        ${item.name}
+                    </div>
+    
+                    <div class="product-rating-container">
+                        <img class="product-rating-stars"
+                        src="images/ratings/rating-${item.rating.stars * 10}.png">
+                        <div class="product-rating-count link-primary">
+                        ${item.rating.count}
+                        </div>
+                    </div>
+    
+                    <div class="product-price">
+                        $${(item.priceCents/100).toFixed(2)}
+                    </div>
+    
+                    <div class="product-quantity-container">
+                        <select>
+                        <option selected value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                        <option value="10">10</option>
+                        </select>
+                    </div>
+    
+                    <div class="product-spacer"></div>
+    
+                    <div class="added-to-cart">
+                        <img src="images/icons/checkmark.png">
+                        Added
+                    </div>
+    
+                    <button class="add-to-cart-button button-primary js-add-to-cart-button">
+                        Add to Cart
+                    </button>
+            </div>
+        `
+    })
+    productsGridElement.innerHTML = productsGridHTML;
+}
+generateProductGrid();
 
-                <div class="product-price">
-                    $${(item.priceCents/100).toFixed(2)}
-                </div>
+const addToCartButtonElements = document.querySelectorAll('.js-add-to-cart-button');
+const cartQuantityElement = document.querySelector('.js-cart-quantity')
 
-                <div class="product-quantity-container">
-                    <select>
-                    <option selected value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="6">6</option>
-                    <option value="7">7</option>
-                    <option value="8">8</option>
-                    <option value="9">9</option>
-                    <option value="10">10</option>
-                    </select>
-                </div>
-
-                <div class="product-spacer"></div>
-
-                <div class="added-to-cart">
-                    <img src="images/icons/checkmark.png">
-                    Added
-                </div>
-
-                <button class="add-to-cart-button button-primary">
-                    Add to Cart
-                </button>
-        </div>
-    `
-})
-
-productsGridElement.innerHTML = productsGridHTML;
+addToCartButtonElements.forEach((button, index) => {
+    button.addEventListener('click', () => {
+            let productFound = false;
+            cart.forEach((element) => {
+                if (element.id === products[index].id) {
+                    element.quantity += 1;
+                    productFound = true;
+                }
+            });
+            if (!productFound) {
+                cart.push({
+                    id: products[index].id,
+                    quantity: 1
+                });
+            }
+        let cartQuantity = 0;
+        cart.forEach(element => cartQuantity += element.quantity);
+        cartQuantityElement.innerHTML = cartQuantity;
+    });
+});
