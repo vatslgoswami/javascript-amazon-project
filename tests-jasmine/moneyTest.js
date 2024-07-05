2f77960430de36135b15ae7cd3f6e5e17f5cd445
vatslgoswami <vatslgoswami@gmail.com>
2024-07-05 22:33:12 +0530

    remove default tests and add money tests in Jasmine framework

---
+++ tests-jasmine/moneyTest.js
@@ -0,0 +1,19 @@
+import { priceFormat } from "../scripts/utils/money.js";
+
+describe('test suite: priceFormat Function', () => {
+    it('converting from cents to dollars', () => {
+        expect(priceFormat(2095)).toEqual('20.95');
+    });
+    describe('edge cases', () =>{
+        it('handling 0', () => {
+            expect(priceFormat(0)).toEqual('0.00');
+        });
+        it('rounding up a number', () => {
+            expect(priceFormat(200.5)).toEqual('2.01');
+        });
+        it('rounding down a number', () => {
+            expect(priceFormat(200.4)).toEqual('2.00');
+        });
+    })
+    
+});
